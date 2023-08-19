693832a7b20bd8589db9705a5b101971268e7c4b
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2023-08-19 01:05:19 -0400

    Migrate to pnpm workspace for monorepo (#1)

---
+++ packages/client/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Workspace } from 'types';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState<Workspace[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/workspaces')
+      .then((response) => response.json())
+      .then(({ data }) => {
+        setData(data);
+      });
+  });
+
+  return (
+    <>
+      <h1 className='font-bold text-red-500 text-6xl'>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p className='text-3xl'>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <h2>Workspaces</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+}
+
+export default App;
